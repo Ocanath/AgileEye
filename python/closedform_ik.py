2e13e92effff30be53555216f345f9b86c1edb0b
Jesse Cornman <ocanath@gmail.com>
2025-04-06 21:25:48 -0700

    more work on closedform ik. arcsin and arccos are making me sad

--- python/closedform_ik.py
+++ python/closedform_ik.py
@@ -64,41 +64,22 @@ def Hz(theta):
 
 
 
-vector_targ_is_normalized = False	#flag for indicating vx,vy,vz are pre-normalized (for clarity in reading the math)
-B, Theta1, Theta2, Theta3, vx, vy, vz, Otx, Oty, Otz, BasePlaneDistance = sp.symbols('"B" "Theta1" "Theta2" "Theta3" ("vx") ("vy") ("vz") "Otx" "Oty" "Otz" "BasePlaneDistance" ',real=True)
-
-
-L0_1 = Hx(-sp.pi/2)
-L0_1[1,3] = -B
-L0_1[2,3] = B
-# sp.pretty_print(L0_1)
-
-L1_2 = Hy(sp.pi/2)
-L1_2[0,3] = -B
-L1_2[2,3] = B
-# sp.pretty_print(L1_2)
-
-H0_1 = Hz(Theta1)*L0_1
-H1_2 = Hz(Theta2)*L1_2
-H0_2 = H0_1*H1_2
-H0_2 = sp.simplify(H0_2)
-# sp.pretty_print(H0_2)
-
-#extremely promising: it appears the 'z' coordinate allows us to 
-sth2 = (Otz/B)-1
-
-cth1_p = sp.sqrt(1 - sth2**2)
-cth1_p = sp.simplify(cth1_p)
-
-cth1_n = -sp.sqrt(1 - sth2**2)
-cth1_n = sp.simplify(cth1_n)
-
-# sp.pretty_print(sth2)
-# sp.pretty_print(cth1_n)
-# sp.pretty_print(cth1_p)
-
+B, Theta1, Theta2, Theta3, vx, vy, vz, Otx, Oty, Otz = sp.symbols('"BasePlaneDistance" "Theta1" "Theta2" "Theta3" ("vx") ("vy") ("vz") "O2Targx" "O2Targy" "O2Targz" ',real=True)
 
 #compute the target o2
+"""
+Rationale of this method:
+
+1. VectTarg normalized is always the z axis of the output coordinate frame.
+2. The x axis will always intersect with the single link rotational axis pin. That means the x axis will be determined by theta
+	of the single link rotational axis/actuator, which is defined as atan2(vx/vy). So it's equal to a unit x vector rotated by Y by that amount.
+	In the case of my robot, it's not actually a unit x, but a negative unit x, because of how I constrained everything, but that's fine.
+3. The y axis, which is where the o2 pin point will be located, is just normal to both of those vectors.
+
+And there you have it.
+"""
+print("-----------------------------------------------------------------------------------------")
+print("Solidworks Equations for setting up O2 and visual references/confirmation of that process\n\n")
 VectTarg = sp.Matrix([vx, vy, vz])
 theta1_arm1 = sp.atan2(vx, vz)
 Ryth = sp.simplify(Ry(theta1_arm1))
@@ -121,9 +102,72 @@ print("\"Rw_3_yx\" = " + sympy_to_solidworks(Rw_3_y[0]))
 print("\"Rw_3_yy\" = " + sympy_to_solidworks(Rw_3_y[1]))
 print("\"Rw_3_yz\" = " + sympy_to_solidworks(Rw_3_y[2]))
 
-O2Targ = -Rw_3_y*BasePlaneDistance	#eye hole is on the opposite side
+O2Targ = -Rw_3_y*B	#eye hole is on the opposite side
 print("\"O2Targx\" = " + sympy_to_solidworks(O2Targ[0]))
 print("\"O2Targy\" = " + sympy_to_solidworks(O2Targ[1]))
 print("\"O2Targz\" = " + sympy_to_solidworks(O2Targ[2]))
+
+
+print("-----------------------------------------------------------------------------------END\n\n")
+
+
+
+
+
+L0_1 = Hx(-sp.pi/2)
+L0_1[1,3] = -B
+L0_1[2,3] = B
+# sp.pretty_print(L0_1)
+
+L1_2 = Hy(sp.pi/2)
+L1_2[0,3] = -B
+L1_2[2,3] = B
+# sp.pretty_print(L1_2)
+
+H0_1 = Hz(Theta1)*L0_1
+H1_2 = Hz(Theta2)*L1_2
+H0_2 = H0_1*H1_2
+H0_2 = sp.simplify(H0_2)
+print("H0_2 for the actuator with two links\r\n-------------------------------------")
+sp.pretty_print(H0_2[0:3,3])
+print("---------------------------------------\n\n")
+
+eq1 = sp.Eq(H0_2[2,3], Otz)
+# sp.pretty_print(eq1)
+theta2_sols = sp.solve(eq1, Theta2)
+# print("\"theta2_s1\" = " + sympy_to_solidworks(theta2_sols[0]))
+# print("\"theta2_s2\" = " + sympy_to_solidworks(theta2_sols[0]))
+xth1 = sp.cos(theta2_sols[0])
+print("\"xth1\" = " + sympy_to_solidworks(xth1))
+yth1 = sp.sin(theta2_sols[0])
+print("\"yth1\" = " + sympy_to_solidworks(yth1))
+theta2_1 = sp.atan2(yth1, xth1)
+print("\"theta2_1\" = " + sympy_to_solidworks(theta2_1))
+# sp.pretty_print(theta2_sols)
+
+print("\n\n")
+
+eq2 = sp.Eq(H0_2[1,3],Oty)
+theta1_sols_y = sp.solve(eq2, Theta1)
+# print("theta1_s1")
+# sp.pretty_print(theta1_sols_y)
+
+
+eq3 = sp.Eq(H0_2[0,3],Otx)
+theta1_sols_x = sp.solve(eq3, Theta1)
+# print("theta1 = ")
+# sp.pretty_print(theta1_sols_x)
+
+
+
+
+#extremely promising: it appears the 'z' coordinate allows us to 
+sth2 = (Otz/B)-1
+
+cth1_p = sp.sqrt(1 - sth2**2)
+cth1_p = sp.simplify(cth1_p)
+
+cth1_n = -sp.sqrt(1 - sth2**2)
+cth1_n = sp.simplify(cth1_n)
 
 
--- python/sympy_to_solidworks_str.py
+++ python/sympy_to_solidworks_str.py
@@ -1,4 +1,4 @@
 import sympy as sp
 
 def sympy_to_solidworks(sympy_expr):
-    return str(sympy_expr).replace('**', '^').replace('sqrt','sqr')
+    return str(sympy_expr).replace('**', '^').replace('sqrt','sqr').replace("asin","arcsin")
